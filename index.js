8bb1f07ac3ec808ee845971a4da44cd1199be3e4
Raguru Narendra Kumar Reddy <110231342+NarendraKumarReddy143@users.noreply.github.com>
2023-11-21 23:05:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
-function minDate() {
-    const today = new Date();
-    return new Date(today.getFullYear() - 55, today.getMonth(), today.getDate()).toISOString().split('T')[0];
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var dobInput = document.getElementById("dob");
+  var dob = new Date(dobInput.value);
+  var termsCheckbox = document.getElementById("termsCheckbox");
+
+  // Additional validation for date of birth
+  var currentDate = new Date();
+  var minDate = new Date(currentDate.getFullYear() - 55, currentDate.getMonth(), currentDate.getDate());
+  var maxDate = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate());
+
+  if (dob < minDate || dob > maxDate) {
+    alert("Please enter a valid date of birth between 18 and 55 years old.");
+    return;
+  }
+
+  // Save the user data
+  var userData = JSON.parse(localStorage.getItem("userData")) || [];
+  userData.push({
+    name: name,
+    email: email,
+    password: password,
+    dob: dobInput.value,
+    acceptedTerms: termsCheckbox.checked
+  });
+
+  localStorage.setItem("userData", JSON.stringify(userData));
+
+  // Display the updated user data in the table
+  loadSavedData();
+
+  // Clear the form fields
+  this.reset();
+});
+
+// Load saved data from web storage on page load
+document.addEventListener("DOMContentLoaded", function() {
+  loadSavedData();
+});
+
+// Load data from web storage
+function loadSavedData() {
+  var userData = JSON.parse(localStorage.getItem("userData")) || [];
+  displayUserData(userData);
 }
 
-function maxDate() {
-    const today = new Date();
-    return new Date(today.getFullYear() - 18, today.getMonth(), today.getDate()).toISOString().split('T')[0];
+// Display user data in the table
+function displayUserData(userData) {
+  var tableBody = document.getElementById("userData");
+  tableBody.innerHTML = "";
+
+  userData.forEach(function(user) {
+    var row = tableBody.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+
+    cell1.innerHTML = user.name;
+    cell2.innerHTML = user.email;
+    cell3.innerHTML = user.password;
+    cell4.innerHTML = user.dob;
+    cell5.innerHTML = user.acceptedTerms ? "Yes" : "No";
+  });
 }
-
-const dobInput = document.getElementById('dob');
-dobInput.setAttribute('min', minDate());
-dobInput.setAttribute('max', maxDate());
-
-let userForm = document.getElementById("userForm");
-
-const getEntries = () => {
-    let entries = localStorage.getItem("userEntries");
-    if (entries) {
-        entries = JSON.parse(entries);
-    } else {
-        entries = [];
-    }
-    return entries;
-}
-
-let userEntries = getEntries();
-
-const dispEntries = () => {
-    const entries = getEntries();
-    const tableEntries = entries.map((entry) => {
-        const name = `<td class="bor">${entry.name}</td>`;
-        const email = `<td class="bor">${entry.email}</td>`;
-        const password = `<td class="bor">${entry.password}</td>`;
-        const dateOfBirth = `<td class="bor">${entry.dob}</td>`; 
-        const atnc = `<td class="bor">${entry.atnc}</td>`;
-
-        const row = `<tr>${name} ${email} ${password} ${dateOfBirth} ${atnc}</tr>`;
-        return row;
-    }).join("\n");
-
-    const table = `<h1>Entries</h1><table class="table"><tr class="bor"><th class="bor">Name</th><th class="bor">Email</th><th class="bor">Password</th><th class="bor">Dob</th><th class="bor">Accepted terms?</th></tr>${tableEntries}</table>`;
-
-    let details = document.getElementById("tableView");
-    details.innerHTML = table;
-}
-
-dispEntries();
-
-const formSubmit = (event) => {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const dateOfBirth = document.getElementById("dob").value; 
-    const atnc = document.getElementById("atnc").checked;
-
-    const entry = {
-        name, email, password, dob: dateOfBirth, atnc 
-    }
-
-    userEntries.push(entry);
-    localStorage.setItem("userEntries", JSON.stringify(userEntries)); 
-    dispEntries();
-}
-
-userForm.addEventListener("submit", formSubmit);
